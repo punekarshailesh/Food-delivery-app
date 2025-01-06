6209ebd41cb172e7f6166b9a8f3db07b719c57cd
shailesh <shaileshmadne29@gmail.com>
2025-01-07 00:34:36 +0530

    Login thing

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@
 
 import Home from "./screens/Home";
 
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./screens/Login";
 
 function App() {
   return (
-    <>
+    <Router>
       <div>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/login" element={<Login/>} />
 
-        <Home></Home>
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
