379bc54f240422094335a3a4c4e356d486d2f62b
shailesh madne <shaileshmadne29@gmail.com>
2025-02-04 12:04:38 +0530

    adding the cart and logout functionality

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,12 +11,12 @@ export default function Card(props) {
       {/* key value pair pass krna padta hai while styling */}
       <div className="card mt-2" style={{ width: "18rem", maxHeight: "360px" }}>
         {/* closing tag is very imp in jsx */}
-        <img src={props.imgSrc} className="card-img-top" alt="Idli" />
+        <img src={props.imgSrc} className="card-img-top" alt="...."  style={{height:"150px",objectFit:"fill"}}/>
         <div className="card-body">
           <h5 className="card-title">
               {props.foodName}
           </h5>
-          <p className="card-text">Food Description</p>
+          <p className="card-text">{props.foodDescription}</p>
 
           <div className="container w-100">
             <select className="m-2 h-100 bg-success rounded">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,26 +21,47 @@ export default function Navbar() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <ul className="navbar-nav">
+            <ul className="navbar-nav me-auto mb-2 ">
               {/* <div className="navbar-nav"> */}
               <li>
-                <Link className="nav-link" aria-current="page" to="/">
+                <Link
+                  className="nav-link active fs-5"
+                  aria-current="page"
+                  to="/"
+                >
                   Home
                 </Link>
               </li>
-              <li>
-                <Link className="nav-link" to="/login">
-                  Login
-                </Link>
-              </li>
 
-              <li>
-                <Link className="nav-link" to="/createuser">
-                  Signup
-                </Link>
-              </li>
-              {/* </div> */}
+              {localStorage.getItem("authToken") ? (
+                <li className="nav-item">
+                  <Link className="nav-link active fs-5" to="/myorder">
+                    My Orders
+                  </Link>
+                </li>
+              ) : (
+                ""
+              )}
             </ul>
+            {!localStorage.getItem("authToken") ? 
+
+            <div className="d-flex">
+              <Link className="btn bg-white text-success mx-1" to="/login">
+                Login
+              </Link>
+
+              <Link className="btn bg-white text-success mx-1" to="/createuser">
+                Signup
+              </Link>
+            </div>
+            :
+            <div>
+            <div className="btn bg-white text-success mx-2">Cart</div>
+            
+
+              <div className="btn bg-white text-success mx-2">Logout</div>
+            </div>
+            }
           </div>
         </div>
       </nav>
