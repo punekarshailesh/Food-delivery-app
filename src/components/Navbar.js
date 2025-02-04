aa40eec431ea28638e0ad2b6e62dcfb8a91ae623
shailesh madne <shaileshmadne29@gmail.com>
2025-02-04 12:16:44 +0530

    navbar functionality changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,18 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { use } from "react";
+import { Link,useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+
+  const navigate = useNavigate();
+  const handleLogout = ()=>{
+    localStorage.removeItem("authToken");
+    navigate("/login")
+    // window.location.replace("/login");
+  }
+
+
+
+
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-success">
@@ -56,10 +67,10 @@ export default function Navbar() {
             </div>
             :
             <div>
-            <div className="btn bg-white text-success mx-2">Cart</div>
+            <div className="btn bg-white text-success mx-2">My Cart</div>
             
 
-              <div className="btn bg-white text-success mx-2">Logout</div>
+              <div className="btn bg-white text-danger mx-2" onClick={handleLogout}>Logout</div>
             </div>
             }
           </div>
